c55740ff4478e5ad55aad5cfdac730e971bb8a06
VSENVY <vanshajsaxena2005@gmail.com>
2023-06-18 15:08:04 +0530

    feat(firenvim): added firenvim
    firenvim for windows support

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,4 +1,5 @@
 return {
+
 	{
 		'christoomey/vim-tmux-navigator',
 		keys = {
@@ -8,4 +9,65 @@ return {
 			{ '<c-l>', desc = 'Move right' },
 		}
 	},
+
+	{
+		'glacambre/firenvim',
+		lazy = false,
+		build = function()
+			require('lazy').load({ plugins = 'firenvim', wait = true })
+			vim.fn['firenvim#install'](0)
+		end,
+		cond = not not vim.g.started_by_firenvim,
+		config = function()
+			vim.g.firenvim_config = {
+				globalSettings = {
+					alt = 'all',
+					cmdlineTimeout = 2001,
+					ignoreKeys = {
+						all = { '<C-->' },
+						normal = { '<C-1>', '<C-2>', '<C-3>', '<C-4>', '<C-5>', '<C-6>', '<C-7>',
+							'<F11>',
+							'<F5>'
+						}
+					}
+				},
+				localSettings = {
+					['.*'] = {
+						cmdline  = 'firenvim',
+						content  = 'text',
+						priority = 0,
+						selector = 'textarea',
+						takeover = 'never'
+					},
+					['google.*'] = {
+						takeover = 'never'
+					}
+				}
+			}
+			--AutoWrite Function
+			function Autowrite_buf_content()
+				vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI' }, {
+					callback = function(e)
+						if vim.g.timer_started == true then
+							return
+						end
+						vim.g.timer_started = true
+						vim.fn.timer_start(10000, function()
+							vim.g.timer_started = false
+							vim.cmd('write')
+						end)
+					end
+				})
+				print('AutoWrite buff content started...')
+				-- code
+			end
+
+			--Keymap for Autowrite_buf_content
+			vim.api.nvim_set_keymap('n', '<leader>aw', '<cmd>lua Autowrite_buf_content()<CR>',
+				{ noremap = true, silent = true })
+			vim.api.nvim_set_keymap('n', '<leader><leader>', '<Cmd>wqall<CR>', {})
+			vim.api.nvim_set_keymap('n', '<esc><leader>', '<Cmd>qall!<CR>', {})
+		end
+	}
+
 }
