fa180c68fbae1340a8b63cde2bf5d23c2cc27d55
Vanshaj Saxena <vs110405@outlook.com>
2023-06-30 01:21:58 +0530

    fix(lspkind): removed lspkind: (doesn't work properly under tmux)
    under tmux vim-pmenu problem isolated, the problem was related
    to lspkind not being maintained for use cases under tmux

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -25,7 +25,6 @@ return {
 			'hrsh7th/cmp-calc',           --Works
 			'hrsh7th/cmp-cmdline',        --Cmdline Completion
 			'saadparwaiz1/cmp_luasnip',   -- Works
-			'onsails/lspkind.nvim',       -- vs_code like pictograms for nvim-cmp
 			'hrsh7th/cmp-nvim-lsp-signature-help', -- Works after completing paran opening, inside paran
 		},
 
@@ -46,22 +45,6 @@ return {
 						end,
 					},
 					formatting = {
-						format = require('lspkind').cmp_format({
-							mode = 'symbol_text', -- show only symbol annotations -- opts: text, text_symbol, symbol_text, symbol
-							maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-							ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-							-- The function below will be called before any actual modifications from lspkind
-							-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-							menu = ({
-								buffer = '[Buffer]',
-								nvim_lsp = '[LSP]',
-								luasnip = '[Luasnip]',
-							}),
-							before = function(entry, vim_item)
-								return vim_item
-							end
-						})
 					},
 					mapping = cmp.mapping.preset.insert({
 						['<C-k>'] = cmp.mapping.scroll_docs(-1),
