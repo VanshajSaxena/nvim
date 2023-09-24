8518e28dc5d1dcc2dff848cf537ed337120ef5bc
VSENVY <vanshajsaxena2005@gmail.com>
2023-09-25 00:28:06 +0530

    chore(keymaps): added desc. keys to keymaps.
    updated sidescrolloff to 48

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,45 +1,56 @@
-local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
 
-local keymap = vim.api.nvim_set_keymap
-
-keymap('', '<Space>', '<Nop>', opts)
+keymap('', '<Space>', '<Nop>', { desc = 'nop' })
 vim.g.mapleader = ' '
 --vim.g.maplocalleader = " "
 
--- Tmux open-float mappings
-if os.getenv('OS') == 'Windows_NT' then
-	keymap('n', '<leader>gg', "<cmd>lua print('you are not under tmux')<cr>", opts)
-	keymap('n', '<leader>*', "<cmd>lua print('you are not under tmux')<cr>", opts)
-	keymap('n', '<leader>H', "<cmd>lua print('you are not under tmux')<cr>", opts)
-else
+function Keymaps()
 	keymap('n', '<leader>gg', ":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -E lazygit<CR>",
-		opts)
-	keymap('n', '<leader>*', ":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -E<CR>", opts)
-	keymap('n', '<leader>H', ":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -E htop<CR>", opts)
+		{ desc = 'tmux open-float lazygit' })
+	keymap('n', '<leader>ti', ":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -E tig<CR>",
+		{ desc = 'tmux open-float tig' })
+	keymap('n', '<leader>*', ":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -E<CR>",
+		{ desc = 'tmux open-float' })
+	keymap('n', '<leader>H', ":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -E htop<CR>",
+		{ desc = 'tmux open-float htop' })
 end
 
+-- Tmux open-float mappings
+if os.getenv('OS') == 'Windows_NT' then
+	keymap('n', '<leader>gg', "<cmd>lua print('you are not under tmux')<cr>", { desc = 'nop' })
+	keymap('n', '<leader>ti', "<cmd>lua print('you are not under tmux')<cr>", { desc = 'nop' })
+	keymap('n', '<leader>*', "<cmd>lua print('you are not under tmux')<cr>", { desc = 'nop' })
+	keymap('n', '<leader>H', "<cmd>lua print('you are not under tmux')<cr>", { desc = 'nop' })
+else
+	Keymaps()
+end
 
-
-keymap('n', '<Tab>', '<C-w>w', opts)
-keymap('n', '<S-Tab>', '<C-w>W', opts)
+keymap('n', '<Tab>', '<C-w>w', { desc = 'tab switching' })
+keymap('n', '<S-Tab>', '<C-w>W', { desc = 'tab switch (shift)' })
 
 -- Highlights under cursor
 if vim.fn.has('nvim-0.9.0') == 1 then
-	vim.keymap.set('n', '<leader>ui', vim.show_pos, { desc = 'Inspect Pos' })
+	keymap('n', '<leader>ui', vim.show_pos, { desc = 'inspect pos' })
 end
 
-vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
-vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
-vim.keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
-vim.keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
-vim.keymap.set('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move down' })
-vim.keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move up' })
-vim.keymap.set('n', 'gb', '<cmd>BufferLinePick<CR>')
-vim.keymap.set('n', 'gD', '<cmd>BufferLinePickClose<CR>')
-vim.keymap.set('n', 'q:', '<cmd>lua print("pressed "q:", aborting")<CR>')
+keymap('n', 'q:', '<nop>', {desc = 'nop'})
+keymap('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'move down' })
+keymap('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'move up' })
+keymap('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'move down (insert)' })
+keymap('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'move up (insert)' })
+keymap('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'move down (visual)' })
+keymap('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'move up (visual)' })
+keymap('n', 'gb', '<cmd>BufferLinePick<CR>', {desc = 'bufferline pick'})
+keymap('n', 'gD', '<cmd>BufferLinePickClose<CR>', {desc = 'bufferline pick close'})
+keymap('n', '<leader>F', vim.lsp.buf.format, {desc = 'lsp format'})
+keymap('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { desc = 'signature help' })
+keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', { desc = 'code action' })
 
-vim.keymap.set('n', '<leader>F', vim.lsp.buf.format)
-vim.keymap.set('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 -- Lazy mappings
-vim.keymap.set('n', '<leader>lp', '<cmd>Lazy profile<cr>')
+keymap('n', '<leader>lp', '<cmd>Lazy profile<cr>', {desc = 'lazy profile'})
+
+-- Jdtls mappings
+keymap('n', '<leader>jr', 'JdtRestart', { desc = 'jdtls restart' })
+keymap('n', '<leader>jwr', 'JdtWipeDataAndRestart', { desc = 'jdtls wipe data and restart' })
+keymap('n', '<leader>juc', 'JdtUpdateConfig', { desc = 'jdtls update config' })
+keymap('n', '<leader>judc', 'JdtUpdateDebugConfig', { desc = 'jdtls update debug config' })
