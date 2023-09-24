c57549e3ac8a798c194edb466d407b19578541f5
Vanshaj Saxena <vs110405@outlook.com>
2023-09-25 01:55:11 +0530

    chore(lsputils): added desc. keys to lsputils

--- lua/plugins/lsp/lsputils.lua
+++ lua/plugins/lsp/lsputils.lua
@@ -34,27 +34,27 @@ return {
 	},
 
 	keys = {
-		{ 'gh',         '<cmd>Lspsaga lsp_finder<cr>' },
-		{ '<leader>gr', '<cmd>Lsapsaga rename<cr>' },
-		{ '<leader>gR', '<cmd>Lspsaga rename ++project<cr>' },
-		{ 'gd',         '<cmd>Lspsaga goto_definition<cr>' },
-		{ 'gd',         '<cmd>Lspsaga peek_definition<cr>' },
---		{ 'sl',         '<cmd>Lspsaga show_line_diagnostics<cr>' },
---		{ 'sc',         '<cmd>Lspsaga show_cursor_diagnostics<cr>' },
---		{ 'sb',         '<cmd>Lspsaga show_buf_diagnostics<cr>' },
-		{ '[e',         '<cmd>Lspsaga diagnostic_jump_prev<cr>' },
-		{ ']e',         '<cmd>Lspsaga diagnostic_jump_next<cr>' },
-		{ 'K',          '<cmd>Lspsaga hover_doc ++quiet<cr>' },
-		{ '<leader>o',  '<cmd>Lspsaga outline<cr>' },
-		{ '<leader>ci', '<cmd>Lspsaga incoming_calls<cr>' },
-		{ '<leader>co', '<cmd>Lspsaga outgoing_calls<cr>' },
-		{ '<leader>tr', '<cmd>Lspsaga term_toggle<cr>' },
+		{ 'gh',         '<cmd>Lspsaga lsp_finder<cr>',           { desc = 'lspsaga finder' } },
+		{ '<leader>gr', '<cmd>Lspsaga rename<cr>',               { desc = 'lspsaga rename' } },
+		{ '<leader>gR', '<cmd>Lspsaga rename ++project<cr>',     { desc = 'lspsaga rename across project' } },
+		{ 'gd',         '<cmd>Lspsaga goto_definition<cr>',      { desc = 'lspsaga goto definition' } },
+		{ 'gd',         '<cmd>Lspsaga peek_definition<cr>',      { desc = 'lspsaga peek definition' } },
+		--		{ 'sl',         '<cmd>Lspsaga show_line_diagnostics<cr>', {desc = 'lspsaga show line diagnostics'}},
+		--		{ 'sc',         '<cmd>Lspsaga show_cursor_diagnostics<cr>', {desc = 'lspsaga show cursor diagnostics'}},
+		--		{ 'sb',         '<cmd>Lspsaga show_buf_diagnostics<cr>', {desc = 'lspsaga show buffer diagnostics'}},
+		{ '[e',         '<cmd>Lspsaga diagnostic_jump_prev<cr>', { desc = 'lspsaga show diagnostics previous' } },
+		{ ']e',         '<cmd>Lspsaga diagnostic_jump_next<cr>', { desc = 'lspsaga show diagnostics next' } },
+		{ 'K',          '<cmd>Lspsaga hover_doc ++quiet<cr>',    { desc = 'lspsaga hover doc' } },
+		{ '<leader>o',  '<cmd>Lspsaga outline<cr>',              { desc = 'lspsaga outline' } },
+		{ '<leader>ci', '<cmd>Lspsaga incoming_calls<cr>',       { desc = 'lspsaga incoming calls (call hierarchy)' } },
+		{ '<leader>co', '<cmd>Lspsaga outgoing_calls<cr>',       { desc = 'lspsaga outgoing calls (call hierarchy)' } },
+		{ '<leader>tr', '<cmd>Lspsaga term_toggle<cr>',          { desc = 'lspsaga terminal toggle' } },
 		{ '[E', function()
 			require('lspsaga.diagnostic'):goto_prev({ severity = vim.diagnostic.severity.ERROR })
-		end },
+		end, { desc = 'lspsaga show error previous' } },
 		{ 'E]', function()
 			require('lspsaga.diagnostic'):goto_next({ severity = vim.diagnostic.severity.ERROR })
-		end },
+		end, { desc = 'lspsaga show error next' } },
 	}
 
 }
