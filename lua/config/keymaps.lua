2341f3d2f5ba5fd7eed43c3c539a5345ee7a49ca
Vanshaj Saxena <vs110405@outlook.com>
2023-09-28 16:39:59 +0530

    fix(keymaps): fixed openfloatTmux function
    changed -E flag position, to prevent it from passing into -T flag agr
    when program is an empty string

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,8 +28,8 @@ end
 if os.getenv('TERM_PROGRAM') == 'tmux' then
 	local openfloatTmux = function(program, map, desc, silent)
 		keymap('n', '<leader>' .. map,
-			":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -b rounded -T " ..
-			program .. ' -E ' .. program .. ' <CR>',
+			":silent !tmux display-popup -d '\\#{pane_current_path}' -w80\\% -h80\\% -b rounded -E -T " ..
+			program .. ' ' .. program .. ' <CR>',
 			{ desc = desc, silent = silent })
 	end
 	openfloatTmux('lazygit', 'gg', 'tmux open-float lazygit', true)
