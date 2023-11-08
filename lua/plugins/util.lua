a12eed03313a1ab24b3c0e421a91964d3f6cc088
Vanshaj Saxena <vs110405@outlook.com>
2023-11-09 00:43:58 +0530

    add(neorg): new plugin neorg
    setup revision needed

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -10,4 +10,25 @@ return {
 		event = 'InsertEnter'
 	},
 
+	{
+		'nvim-neorg/neorg',
+		build = ':Neorg sync-parsers',
+		ft = 'neorg',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			require('neorg').setup {
+				load = {
+					['core.defaults'] = {}, -- Loads default behaviour
+					['core.concealer'] = {}, -- Adds pretty icons to your documents
+					['core.dirman'] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = '~/notes',
+							},
+						},
+					},
+				},
+			}
+		end,
+	}
 }
