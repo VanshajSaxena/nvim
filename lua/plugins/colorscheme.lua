ad5c8a8149af2f5710bd215eaddac050175619e8
Vanshaj Saxena <vs110405@outlook.com>
2024-01-31 21:43:32 +0530

    feat(transparency): add transparent.nvim and refactor gruvbox config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,25 +7,40 @@ return {
 		priority = 1000,
 		config = function()
 			require('gruvbox').setup({
-				contrast = 'hard',
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = true,
 				italic = {
-					strings   = true,
-					comments  = true,
-					operators = false,
-					folds     = true,
+					strings = true,
 					emphasis = true,
+					comments = true,
+					operators = false,
+					folds = true,
 				},
+				strikethrough = true,
+				invert_selection = true,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = 'hard', -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = true,
 			})
-			vim.cmd([[colorscheme gruvbox]])
-			vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
-			vim.cmd([[hi FidgetTask ctermbg=NONE guibg=NONE guifg=#504945]])
-			vim.cmd([[hi FloatBorder ctermbg=NONE guibg=NONE ]])
-			vim.cmd([[hi WinBar ctermbg=NONE guibg=NONE ]])
-			vim.cmd([[hi @keyword cterm=italic gui=italic guifg=#fb4934]])
-			vim.cmd([[hi link SagaNormal Normal]])
-			vim.cmd([[hi link LazyNormal Normal]])
-			vim.cmd([[hi link @keyword.return.lua @keyword ]])
+			vim.cmd([[ colorscheme gruvbox ]])
+			vim.cmd([[ hi @keyword cterm=italic gui=italic guifg=#fb4934 ]])
+			vim.cmd([[ hi link SagaNormal Normal ]])
+			vim.cmd([[ hi link LazyNormal Normal ]])
+			vim.cmd([[ hi link @keyword.return.lua @keyword ]])
 		end
+	},
+
+	{
+		'xiyaowong/transparent.nvim',
+		lazy = false,
 	},
 
 	{
@@ -34,9 +49,7 @@ return {
 		lazy = true,
 		--priority = 1000,
 		config = function()
-			--vim.cmd([[colorscheme tokyonight-night ]])
-			vim.cmd([[ hi Normal guibg=NONE ctermbg=NONE]])
-			vim.cmd([[hi NormalFloat guibg=NONE ctermbg=NONE]])
+			vim.cmd([[colorscheme tokyonight-night ]])
 		end
 	}
 
