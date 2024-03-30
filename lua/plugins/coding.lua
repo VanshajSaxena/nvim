b4fae1d452fce41639e1ee460218b613d33735f0
Vanshaj Saxena <vs110405@outlook.com>
2024-03-30 17:58:59 +0530

    fix(nvim-cmp): completion fix

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -18,11 +18,13 @@ return {
     },
 
     opts = function(_, opts)
+      --[[
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
+      --]]
 
       local luasnip = require("luasnip")
       local cmp = require("cmp")
@@ -63,25 +65,26 @@ return {
       }
       opts.sources = vim.tbl_extend("force", opts.sources, {
         { name = "emoji" },
+        { name = "calc" },
       })
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.scroll_docs(-1),
+        ["<C-j>"] = cmp.mapping.scroll_docs(1),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(function(fallback) -- Tab completion
           if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
+            cmp.select_next_item() -- set select_opts
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
           else
             fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_prev_item()
+            cmp.select_prev_item() -- set select_opts
           elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
