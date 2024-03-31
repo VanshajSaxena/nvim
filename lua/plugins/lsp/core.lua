6bc02e75ae4699c688002ac3ae9f5e2edc209acd
Vanshaj Saxena <vs110405@outlook.com>
2024-03-31 23:28:17 +0530

    fix(sourcekit):mod key for cmd

--- lua/plugins/lsp/core.lua
+++ lua/plugins/lsp/core.lua
@@ -9,19 +9,21 @@ return {
     opts = {
       servers = {
         sourcekit = {
-          cmd = vim.tbl_extend("force", {}, vim.loop.os_uname().sysname == "Darwin" and {
-            "sourcekit-lsp",
-            "-Xswiftc",
-            "-sdk",
-            "-Xswiftc",
-            "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk",
-            "-Xswiftc",
-            "-target",
-            "-Xswiftc",
-            "arm64-apple-ios15.0-simulator",
-            "-Xcc",
-            "-DSWIFT_PACKAGE=0",
-          } or { "sourcekit-lsp" }),
+          cmd = vim.loop.os_uname().sysname == "Darwin"
+              and {
+                "sourcekit-lsp",
+                "-Xswiftc",
+                "-sdk",
+                "-Xswiftc",
+                "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk",
+                "-Xswiftc",
+                "-target",
+                "-Xswiftc",
+                "arm64-apple-ios15.0-simulator",
+                "-Xcc",
+                "-DSWIFT_PACKAGE=0",
+              }
+            or { "sourcekit-lsp" },
         },
         nil_ls = {},
         clangd = {},
