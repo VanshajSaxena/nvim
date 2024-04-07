c1e77d9a8772b0c20453f25c40dd8679a7851830
Vanshaj Saxena <vs110405@outlook.com>
2024-04-07 21:41:36 +0530

    feat(autocmds): new autocmd for swift filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,23 +5,31 @@
 local function augroup(name)
   return vim.api.nvim_create_augroup("augroup_" .. name, { clear = true })
 end
---:au filetype c,cpp,java set mps+==:;
+
 vim.api.nvim_create_autocmd("filetype", {
   group = augroup("matchpairs"),
   pattern = { "c", "cpp", "java" },
   command = "set mps+==:;",
+  desc = "set matchpairs for c, cpp, java",
 })
 
--- touchegg files are xml types
 vim.api.nvim_create_autocmd({ "bufreadpost", "bufnewfile" }, {
   group = augroup("touchegg_xml"),
   pattern = vim.fn.expand("$home") .. "/.config/touchegg/*",
   command = "set filetype=xml",
+  desc = "touchegg files are xml types",
 })
 
--- managing comment options
 vim.api.nvim_create_autocmd("filetype", {
   group = augroup("formatoptions"),
   pattern = "*",
   command = "set formatoptions+=t | set formatoptions-=cro",
+  desc = "set formatoptions to avoid ambiguous comment behavior",
+})
+
+vim.api.nvim_create_autocmd("filetype", {
+  group = augroup("swift_playground_setwrap"),
+  pattern = { "swift" },
+  command = "set wrap",
+  desc = "swift playground files set wrap",
 })
