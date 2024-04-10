c88f57edbf49b46fed51cbe4f19450bc1baf7e2f
Vanshaj Saxena <vs110405@outlook.com>
2024-04-10 21:28:36 +0530

    new(xbase): xbase for xcodeproj

--- lua/plugins/lsp/core.lua
+++ lua/plugins/lsp/core.lua
@@ -29,5 +29,18 @@ return {
         clangd = {},
       },
     },
+
+    {
+      "xbase-lab/xbase",
+      enabled = true,
+      build = "make install", -- or "make install && make free_space" (not recommended, longer build time)
+      dependencies = {
+        "neovim/nvim-lspconfig",
+        -- "nvim-telescope/telescope.nvim", -- optional
+        -- "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
+        -- "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+      },
+      opts = {},
+    },
   },
 }
