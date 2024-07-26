693bbb83c196b24ab748326e5d5cf1355adb860e
Vanshaj Saxena <vs110405@outlook.com>
2024-07-26 23:20:50 +0530

    updated neorg config: luarocks remove as dependency

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,19 +1,7 @@
 return {
 
   {
-
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    opts = {
-      luarocks_build_args = {
-        "--with-lua-include=/usr/include",
-      },
-    },
-  },
-
-  {
     "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
     config = function()
@@ -23,9 +11,6 @@ return {
           ["core.concealer"] = {},
         },
       })
-
-      vim.wo.foldlevel = 99
-      vim.wo.conceallevel = 2
     end,
   },
 }
