6ae885b96225ed83e292079b9629269f9ecdffe7
Vanshaj Saxena <vs110405@outlook.com>
2024-11-19 19:42:05 +0530

    leetcode: removed optional dependencies

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -6,11 +6,6 @@ return {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim", -- required by telescope
     "MunifTanjim/nui.nvim",
-
-    -- optional
-    "nvim-treesitter/nvim-treesitter",
-    "rcarriga/nvim-notify",
-    -- "nvim-tree/nvim-web-devicons",
   },
   opts = {
     lang = "java",
