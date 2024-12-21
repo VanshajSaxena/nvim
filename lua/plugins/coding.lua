06812ecf11ee150bb0ab47035810c3bbddac9ea5
Vanshaj Saxena <vs110405@outlook.com>
2024-12-21 16:47:10 +0530

    switch to blink.cmp
    removed nvim-cmp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,87 +1,29 @@
 return {
 
   {
-    "hrsh7th/nvim-cmp",
-    event = { "InsertEnter", "CmdLineEnter" },
-    dependencies = {
-      "hrsh7th/cmp-calc", -- Works
-      "hrsh7th/cmp-cmdline", -- Cmdline Completion
-      "hrsh7th/cmp-emoji", -- emoji Completion
+    "seghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded",
+          winblend = 0,
+          draw = {
+            columns = { { "label", "label_description", gap = 5 }, { "kind_icon", "kind" } },
+          },
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+        },
+      },
+      signature = {
+        enabled = true,
+        window = {
+          border = "rounded",
+        },
+      },
     },
-
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local cmp = require("cmp")
-      opts.view = {
-        entries = {
-          follow_cursor = true,
-        },
-      }
-
-      opts.window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      }
-
-      opts.sources = {
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "calc" },
-        { name = "emoji" },
-        { name = "copilot" },
-        { name = "neorg" },
-      }
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-      })
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "cmdline" }, -- Good
-          { name = "path" }, -- Good
-        },
-      })
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" }, -- Good
-          { name = "path" }, -- Good
-        },
-      })
-    end,
   },
 
   {
