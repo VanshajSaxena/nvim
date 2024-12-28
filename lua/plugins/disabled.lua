841e9eb50e791e42656483161d4e9c0a6bcb4a08
Vanshaj Saxena <vs110405@outlook.com>
2024-12-28 23:38:14 +0530

    remove disabled entries as they are not enabled by default

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,16 +1,7 @@
 return {
-  {
-    "nvimdev/dashboard-nvim",
-    enabled = false,
-  },
 
   {
     "folke/noice.nvim",
-    enabled = false,
-  },
-
-  {
-    "mini.animate",
     enabled = false,
   },
 }
