2d4c7bf8bea34ad816fdc469831f57fd08f2071b
Vanshaj Saxena <vs110405@outlook.com>
2025-02-17 16:35:06 +0530

    remove old commented mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,26 +8,3 @@ keymap("n", "<leader>lE", "<cmd>LazyExtras<cr>", { desc = "Lazy Extras" })
 
 keymap("i", "<C-l>", "<right>", { desc = "move right in insert mode" })
 keymap("i", "<C-j>", "<down>", { desc = "move down in insert mode" })
-
-
--- stylua: ignore
-
---[[
-if os.getenv("TERM_PROGRAM") == "tmux" then
-  local openfloat = function(program, title, map, desc, silent)
-    -- stylua: ignore
-    keymap( "n", "<leader>" .. map, ":silent !tmux display-popup -T " .. title .. " -d '\\#{pane_current_path}' -w80\\% -h80\\% -b rounded -E " .. program .. "<cr>", { desc = desc, silent = silent })
-  end
-  -- openfloat("lazygit", '"   "', "gg", "tmux open-float lazygit", true)
-  openfloat("", '"   "', ">", "tmux open-float term", true)
-  openfloat("tig", '" tig "', "ti", "tmux open-float tig", true)
-  openfloat("htop", '" htop "', "H", "tmux open-float htop", true)
-  openfloat("btop", '" btop "', "B", "tmux open-float btop", true)
-elseif os.getenv("OS") == "Windows_NT" then
-  local openfloat = function(program, map, desc, silent)
-    keymap("n", "<leader>" .. map, "<cmd>Lspsaga term_toggle " .. program .. "<cr>", { desc = desc, silent = silent })
-  end
-  -- openfloat("lazygit", "gg", "Lspsaga term_toggle lazygit", true)
-  openfloat("pwsh", ">", "Lspsaga term_toggle", true)
-end
---]]
