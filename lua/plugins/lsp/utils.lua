2dcf6ed89f5fbef2f551aab3176db6cd286e000d
Vanshaj Saxena <vs110405@outlook.com>
2025-03-15 19:13:44 +0530

    lspsaga: finder cmd and keymap

--- lua/plugins/lsp/utils.lua
+++ lua/plugins/lsp/utils.lua
@@ -6,13 +6,16 @@ return {
 
     opts = {
       finder = {
-        default = "def+imp",
+        -- default = "ref+imp",
         keys = {
           shuttle = "<C-w>w",
           vsplit = "<C-v>",
           split = "<C-s>",
           tabe = "<C-t>",
           toggle_or_open = "<cr>",
+        },
+        methods = {
+          tyd = "textDocument/typeDefinition",
         },
       },
       callhierarchy = {
@@ -53,13 +56,14 @@ return {
 
     -- stylua: ignore
     keys = {
-      { "gq", "<cmd>Lspsaga finder ++inexist<cr>", desc = "lspsaga finder" },
-      { "gd", "<cmd>Lspsaga goto_definition<cr>", desc = "lspsaga goto definition" },
-      { "<leader>gp", "<cmd>Lspsaga peek_definition<cr>", desc = "lspsaga peek definition" },
-      { "K", "<cmd>Lspsaga hover_doc ++quiet<cr>", desc = "lspsaga hover doc" },
-      { "<leader>o", "<cmd>Lspsaga outline<cr>", desc = "lspsaga outline" },
-      { "<leader>ci", "<cmd>Lspsaga incoming_calls<cr>", desc = "lspsaga incoming calls (call hierarchy)", },
-      { "<leader>co", "<cmd>Lspsaga outgoing_calls<cr>", desc = "lspsaga outgoing calls (call hierarchy)", },
+      { "gh", "<cmd>Lspsaga finder def+imp<cr>", desc = "Lspsaga (definitions + implementations)" },
+      { "gq", "<cmd>Lspsaga finder ref+tyd<cr>", desc = "Lspsaga (references + type definitions)" },
+      { "gd", "<cmd>Lspsaga goto_definition<cr>", desc = "Lspsaga goto definition" },
+      { "<leader>gp", "<cmd>Lspsaga peek_definition<cr>", desc = "Lspsaga peek definition" },
+      { "K", "<cmd>Lspsaga hover_doc ++quiet<cr>", desc = "Lspsaga hover doc" },
+      { "<leader>o", "<cmd>Lspsaga outline<cr>", desc = "Lspsaga outline" },
+      { "<leader>ci", "<cmd>Lspsaga incoming_calls<cr>", desc = "Lspsaga incoming calls (call hierarchy)" },
+      { "<leader>co", "<cmd>Lspsaga outgoing_calls<cr>", desc = "Lspsaga outgoing calls (call hierarchy)" },
     },
   },
 }
