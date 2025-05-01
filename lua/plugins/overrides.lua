06c604156a5c68387f1834f54a3cb02679ab51f0
Vanshaj Saxena <vs110405@outlook.com>
2025-05-01 17:17:39 +0530

    feat(plugins): add vimtex plugin for tex files

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -213,4 +213,14 @@ return {
       },
     },
   },
+
+  {
+    "lervag/vimtex",
+    config = function()
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+      vim.g.vimtex_view_method = "sioyek"
+      vim.g.vimtex_compiler_method = "tectonic"
+    end,
+  },
 }
