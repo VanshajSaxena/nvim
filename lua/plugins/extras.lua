9f5085cfaf055f2a8ea75547a034327a9ecffb71
Vanshaj Saxena <vs110405@outlook.com>
2025-05-04 18:44:41 +0530

    feat(plugins): add kitty-scrollback

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -120,4 +120,20 @@ return {
       })
     end,
   },
+
+  {
+    "mikesmithgh/kitty-scrollback.nvim",
+    enabled = true,
+    lazy = true,
+    cmd = {
+      "KittyScrollbackGenerateKittens",
+      "KittyScrollbackCheckHealth",
+      "KittyScrollbackGenerateCommandLineEditing",
+    },
+    event = { "User KittyScrollbackLaunch" },
+    version = "*",
+    config = function()
+      require("kitty-scrollback").setup()
+    end,
+  },
 }
