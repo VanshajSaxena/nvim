9ae2bb849c2363d711dd39b96a2da3795a729b8c
Vanshaj Saxena <vs110405@outlook.com>
2025-07-03 00:20:14 +0530

    fix(neovide): disable neovide cursor smooth blink

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -34,6 +34,7 @@ end
 
 if vim.g.neovide then
   vim.g.neovide_opacity = 1
+  vim.g.neovide_cursor_smooth_blink = false
   vim.o.guifont = "FiraCode Nerd Font:h12"
   set.pumblend = 0 -- Popup blend pumblend has to be set to zero for : completion
   set.winblend = 0 -- Float-windows blend
