7847fd0f3688a8ef3236b41854559fcc629694bc
Vanshaj Saxena <vs110405@outlook.com>
2025-07-02 23:57:25 +0530

    fix(keymaps): disable `H` and `K`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,8 @@ local keymap = vim.keymap.set
 vim.keymap.del("n", "<leader>l") -- Disable Lazy Home
 vim.keymap.del("n", "<leader>L") -- Disable Lazyvim changelog
 vim.keymap.del({ "n", "i", "x" }, "<C-s>")
+vim.keymap.del("n", "H")
+vim.keymap.del("n", "L")
 
 keymap("n", "<leader>lp", "<cmd>Lazy<cr>", { desc = "Lazy Profile" })
 keymap("n", "<leader>lE", "<cmd>LazyExtras<cr>", { desc = "Lazy Extras" })
