c97ba50f49a6f7aa6f4a7307986c4f45c0116057
Vanshaj Saxena <vs110405@outlook.com>
2025-07-18 10:33:44 +0530

    revert(refactor): segregate options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,6 +16,36 @@ set.scroll = 10 -- scroll for CTRL-D and CTRL-U
 set.scrolloff = 16
 set.sidescrolloff = 48
 
+if os.getenv("OS") == "Windows_NT" then
+  vim.cmd([[ let g:clipboard = {
+\ 'name': 'win32yank',
+\ 'copy': {
+\    '+': 'win32yank.exe -i --crlf',
+\    '*': 'win32yank.exe -i --crlf',
+\  },
+\ 'paste': {
+\    '+': 'win32yank.exe -o --lf',
+\    '*': 'win32yank.exe -o --lf',
+\ },
+\ 'cache_enabled': 0,
+\ }
+]])
+  vim.o.shell = "powershell.exe"
+  vim.o.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
+
+  -- Get the user's home directory
+  local home = os.getenv("USERPROFILE") or os.getenv("HOME")
+
+  -- Construct the Scoop shims path
+  local scoop_shims_path = home .. "\\scoop\\shims"
+  vim.env.PATH = scoop_shims_path .. ";" .. vim.env.PATH
+end
+
 if vim.g.neovide then
   vim.g.neovide_opacity = 1
   vim.g.neovide_cursor_smooth_blink = false
