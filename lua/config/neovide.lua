d97d3d800b5cb0f248e564d675c5afbb1e460293
Vanshaj Saxena <vs110405@outlook.com>
2025-07-23 08:22:36 +0530

    refactor: require neovide

---
+++ lua/config/neovide.lua
@@ -0,0 +1,8 @@
+if vim.g.neovide then
+  vim.g.neovide_opacity = 1
+  vim.g.neovide_cursor_smooth_blink = false
+  vim.o.guifont = "FiraCode Nerd Font:h12"
+  vim.o.pumblend = 0 -- Popup blend pumblend has to be set to zero for : completion
+  vim.o.winblend = 0 -- Float-windows blend
+  vim.env.EDITOR = "neovide"
+end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,15 +46,7 @@ if os.getenv("OS") == "Windows_NT" then
   vim.env.PATH = scoop_shims_path .. ";" .. vim.env.PATH
 end
 
-if vim.g.neovide then
-  vim.g.neovide_opacity = 1
-  vim.g.neovide_cursor_smooth_blink = false
-  vim.g.neovide_fullscreen = true
-  vim.o.guifont = "FiraCode Nerd Font:h11"
-  set.pumblend = 0 -- Popup blend pumblend has to be set to zero for : completion
-  set.winblend = 0 -- Float-windows blend
-end
-
+require("config.neovide")
 -- OpenAPI filetype
 vim.filetype.add({
   pattern = {
